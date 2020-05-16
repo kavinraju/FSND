d143240be094416eac926a1bf84a1de5b2454a0e
kavinraju <rajukavin25@gmail.com>
2020-05-16 13:12:18 +0530

    configured for heroku deployment

---
+++ models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+database_name = "demodb"
+username = "postgres"
+password = 'skr123'
+database_path = "postgres://{}:{}@{}/{}".format(username, password, 'localhost:5432', database_name)
+
+# database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
+
+  id = Column(db.Integer, primary_key=True)
+  name = Column(db.String)
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'catchphrase': self.catchphrase}
